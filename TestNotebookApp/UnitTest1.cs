afb159e089032c1cd284cd4004fe9d72a16ddf26
Sulaiman Kasas <s195462@dtu.dk>
2021-05-12 19:30:06 +0200

    added xunitTest

---
+++ TestNotebookApp/UnitTest1.cs
@@ -0,0 +1,28 @@
+using System;
+using Xunit;
+
+namespace TestNotebookApp
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestCalc()
+        {
+            //test addition function
+            double additions = NoteApp.Model.MathLogic.Addition(20,5);
+            Assert.Equal("25", additions + "");
+
+            //test addition function
+            double multiplications = NoteApp.Model.MathLogic.Multiplication(5, 5);
+            double admod = additions + multiplications;
+            Assert.Equal("50", admod + "");
+
+        }
+
+        [Fact]
+        public void TestNote()
+        {
+           
+        }
+    }
+}
